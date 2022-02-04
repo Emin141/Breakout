7228684d4c5a2b2edd6ac62a37a51325ea9708f3
Emin Turalic <emin141@outlook.com>
2022-02-04 20:11:23 +0100

    Add project files.

---
+++ Breakout/Source/Game.cpp
@@ -0,0 +1,24 @@
+#include "Game.h"
+
+Game::Game(void) {
+	// Constructor call
+}
+
+void Game::Initialize(void) {
+	// Init SDL and XML parsing
+	// Load assets
+	// Setup scenes and actors
+}
+
+void Game::Run(void) {
+	// Logic
+	// Render
+	// Poll events
+}
+
+void Game::Quit(void) {
+	// Unload assets
+	// Deallocate all dynamic objects
+	// Quit SDL and XML parsing
+	// Return 0;
+}
---
+++ Breakout/Source/Include/Game.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Game {
+public:
+	Game(void);
+	void Initialize(void);
+	void Run(void);
+	void Quit(void);
+private:
+	// We'll see
+};
