fdf5f2f0e31ba7df12e9fb7c864def8c87bb61eb
Gridimlin <76913416+Emin141@users.noreply.github.com>
2022-02-07 20:05:18 +0100

    Small add to game.h

--- Breakout/Source/Include/Game.h
+++ Breakout/Source/Include/Game.h
@@ -17,13 +17,22 @@ private:
 	void Draw(); // Will be modified when scenes are added
 	void Poll(); // Polls events
 private:
+	// Loading, Menu, GameOver, and Level
 	GameState mGameState;
+
+	// Window related stuff
 	sf::RenderWindow mWindow;
 	unsigned int mWindowWidth;
 	unsigned int mWindowHeight;
 
+	// Global font, used in all assets
 	sf::Font mFont;
 
+	// Mouse condition
+	sf::Vector2i mMousePosition;
+	bool cliked;
+
+	// Scenes, all inherited from Scene
 	LoadingScreen mLoadingScreen;
 	Menu mMenu;
 	Scene mGameOver;
