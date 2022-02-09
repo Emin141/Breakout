e38ce10790291db415ef1b2c37ccf3b4c0d0a699
Gridimlin <76913416+Emin141@users.noreply.github.com>
2022-02-09 17:08:13 +0100

    XML parsing works without problems!

--- Breakout/Source/Include/Brick.h
+++ Breakout/Source/Include/Brick.h
@@ -1,34 +1,27 @@
 #pragma once
 
+#include <SFML/Audio.hpp>
 #include "Entity.h"
-#include <SDL_audio.h>
+#include "tinyxml2.h"
 
-enum class BrickType {
-	SOFT = 0,
-	MEDIUM,
-	HARD,
-	IMPENETRABLE
-};
+// Bricks will have to be redone later with pointers
+// The SoundBuffers and Textures of each brick type needs to be 
+// in the level object, and then passed as a pointer to the bricks
 
 class Brick : Entity {
-//	The brick class inherits Entity which has a CollisionBox
-// 	and a ToBeRemoved flag. However, Entity inherits Actor,
-// 	which is the class that has the dimensions, screen position
-// 	image associated with the Brick, and ID to track which 
-// 	brick it is (each entity has a unique ID). 
-// 	
-// 	The Actor grandparent also has the Load and Draw calls
-// 	which mush be used by Entity. The Draw call can be inherited
-// 	directly, however the Load call needs to be reimplemented
-// 	as an overridden function
-// 
 public:
-	
+    void setAttributes(const tinyxml2::XMLElement* brickTypeElement);
+
+    void playHitSound();
+    //void playBreakSound();
+    inline bool isDead() { return mHitPoints == 0 ? true : false; }
+    inline void debugPrint() const { std::cout << mHitPoints; }
 private:
-	BrickType mBrickType;
-	// Texture is implemented in parent as SDL_Surface*
-	uint8_t mHitPoints;
-	// SDL Audio Hit
-	// SDL Audio break
-	uint16_t mBreakScore;
+    char* mIdentifier;
+    sf::Texture mTexture;
+    unsigned int mHitPoints;
+    sf::Sound mHitSound, mBreakSound;
+    sf::SoundBuffer mHitSoundBuffer, mBreakSoundBuffer;
+    unsigned int mBreakScore;
 };
+
--- Breakout/Source/Include/Level.h
+++ Breakout/Source/Include/Level.h
@@ -1,24 +1,47 @@
 #pragma once
 
+#include <list>
+#include <array>
+
 #include "Scene.h"
 #include "Paddle.h"
 #include "Ball.h"
+#include "Brick.h"
+
+enum class BrickType {
+	SOFT = 0,
+	MEDIUM = 1,
+	HARD = 2,
+	IMPENETRABLE = 3,
+	SIZE = 4
+};
 
 class Level : public Scene {
 public:
 	Level();
-	// Temporary load method
-	void Load(const sf::Vector2i& paddlePosition);
-	// Method to load from XML file
-	// Method to draw the scene
+	void LoadFromXML(const std::string&, const sf::RenderWindow&);
+	//void ArrangeBricks();
 	void Draw(sf::RenderWindow& window);
 	void Update(const sf::Vector2i& mousePosition, const sf::RenderWindow& window, const float dt);
 
 private:
-	// Paddle, ball, and some container for the bricks
-	// Maybe the brick layout passed from XML?
-	sf::Texture mPaddleTexture, mBallTexture;
+	// Paddle info
+	sf::Texture mPaddleTexture;
 	Paddle mPaddle;
+
+	// Ball info
+	sf::Texture mBallTexture;
 	Ball mBall;
+
+	// Bricks info
+	unsigned int mRowCount;
+	unsigned int mColumnCount;
+	unsigned int mRowSpacing;
+	unsigned int mColumnSpacing;
+	sf::Texture mBackgoundTexture;
+	std::array<Brick, 4> mBrick;
+	std::string mBrickLayout;
+
+	std::list<Brick> mBrickList;
 };
 
